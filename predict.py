ede6849a648197280b4becf5de97ef66e67fc0a2
Iem-Prog <btoxic24@gmail.com>
2018-03-25 21:23:42 +0100

    Solving resizing img error

--- predict.py
+++ predict.py
@@ -7,44 +7,67 @@ import numpy as np
 classifier = joblib.load("digits_classifier.pkl")
 
 #Testing it
-img = cv2.imread("digit1")
-#cap = cv2.VideoCapture(0)
+img = cv2.imread("digit1.jpg")
 
+output = ""
+if img is not None:
+    #Convert image to grayscale and applying filters
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
 
-#Convert image to grayscale and applying filters
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    #Creating a threshold
+    ret, img_th = cv2.threshold(img_gray, 90, 255, cv2.THRESH_BINARY_INV)
 
-#Creating a threshold
-ret, img_th = cv2.threshold(img_gray, 90, 255, cv2.THRESH_BINARY_INV)
+    #Find contours in the image
+    image, ctrs, hier = cv2.findContours(img_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#Find contours in the image
-ctrs, hier = cv2.findContours(img_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #Get rectangles contains each contour
+    #rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+    for c in ctrs:
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(c)
+        # draw a green rectangle to visualize the bounding rect
+        #cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # get the min area rect
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+        box = np.array(box, dtype="int")
 
-#Get rectangles contains each contour
-rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+        # convert all coordinates floating point values to int
+        box = np.int0(box)
+        # draw a red 'nghien' rectangle
+        cv2.drawContours(img, [box], 0, (0, 0, 255))
 
-# For each rectangular region, calculate HOG features and predict
-# the digit using Linear SVM.
-for rect in rects:
-    # Draw the rectangles
-    cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
-    # Make the rectangular region around the digit
-    leng = int(rect[3] * 1.6)
-    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-    roi = img_th[pt1:pt1+leng, pt2:pt2+leng]
+        # finally, get the min enclosing circle
+        #(x, y), radius = cv2.minEnclosingCircle(c)
+        # convert all values to int
+        #center = (int(x), int(y))
+        #radius = int(radius)
+        # and draw the circle in blue
+        #img = cv2.circle(img, center, radius, (255, 0, 0), 2)
 
-    # Resize the image
-    roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-    """ a pixel element is ‘1’ if atleast one pixel under the kernel is ‘1’.
-    So it increases the white region in the image or size of foreground object increases"""
-    roi = cv2.dilate(roi, (3, 3))
+        roi = img_th[y:y+h, x:x+w]
+        # Resize the image
+        roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        #a pixel element is ‘1’ if atleast one pixel under the kernel is ‘1’.
+        #So it increases the white region in the image or size of foreground object increases
+        roi = cv2.dilate(roi, (3, 3))
 
-    # Calculate the HOG features
-    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-    nbr = classifier.predict(np.array([roi_hog_fd], 'float64'))
-    cv2.putText(img, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+        # Calculate the HOG features
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+        nbr = classifier.predict(np.array([roi_hog_fd], 'float64'))
+        output = output +" "+str(nbr[0])
+        cv2.putText(img, str(int(nbr[0])), (x, y), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255), 3)
+    cv2.imshow("Predicted digits within the image", img)
+    print("Output: ", output)             #Contours goes from right to left
 
-cv2.imshow("Predicted digits within the image", img)
-cv2.waitKey()
+#print("Number of contours is: ", len(ctrs))
+#cv2.drawContours(img, ctrs, -1, (255, 255, 0), 1)
+
+#cv2.imshow("contours", img)
+ESC = 27
+while True:
+    keycode = cv2.waitKey()
+    if keycode == ESC:
+        break
+cv2.destroyAllWindows()
--- real_time_setup.py
+++ real_time_setup.py
@@ -8,80 +8,66 @@ classifier = joblib.load("digits_classifier.pkl")
 
 #Testing it
 cap = cv2.VideoCapture(0)
-while(cap.isOpened()):  # check !
+while(True):  # check !
     # capture frame-by-frame
-    ret, frame = cap.read()
-
-    if ret: # check ! (some webcam's need a "warmup")
-        # our operation on frame come here
-        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, img = cap.read()
+    if img is not None:
+        #Convert image to grayscale and applying filters
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
 
         #Creating a threshold
         ret, img_th = cv2.threshold(img_gray, 90, 255, cv2.THRESH_BINARY_INV)
 
         #Find contours in the image
-        ctrs, hier, _ = cv2.findContours(img_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        image, ctrs, hier = cv2.findContours(img_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         #Get rectangles contains each contour
-        rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+        #rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+        for c in ctrs:
+            # get the bounding rect
+            x, y, w, h = cv2.boundingRect(c)
+            # draw a green rectangle to visualize the bounding rect
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # get the min area rect
+            #rect = cv2.minAreaRect(c)
+            #box = cv2.boxPoints(rect)
+            #box = np.array(box, dtype="int")
 
-        # For each rectangular region, calculate HOG features and predict
-        # the digit using Linear SVM.
-        for rect in rects:
-            #Draw the rectangles
-            cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
-            # Make the rectangular region around the digit
-            leng = int(rect[3] * 1.6)
-            pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-            pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-            roi = img_th[pt1:pt1+leng, pt2:pt2+leng]
+            # convert all coordinates floating point values to int
+            #box = np.int0(box)
 
+            # finally, get the min enclosing circle
+            #(x, y), radius = cv2.minEnclosingCircle(c)
+
+            # convert all values to int
+            #center = (int(x), int(y))
+            #radius = int(radius)
+            # and draw the circle in blue
+            #img = cv2.circle(img, center, radius, (255, 0, 0), 2)
+
+            roi = img_th[y:y+h, x:x+w]
             # Resize the image
-            def split_up_resize(arr, res):
-                """
-                function which resizes large array (direct resize yields error (addedtypo))
-                """
-
-                # compute destination resolution for subarrays
-                res_1 = (res[0], res[1]//2)
-                res_2 = (res[0], res[1] - res[1]//2)
-
-                # get sub-arrays
-                arr_1 = arr[0:len(arr)//2]
-                arr_2 = arr[len(arr)//2:]
-
-                # resize sub arrays
-                arr_1 = cv2.resize(arr_1, res_1, interpolation = cv2.INTER_LINEAR)
-                arr_2 = cv2.resize(arr_2, res_2, interpolation = cv2.INTER_LINEAR)
-
-                # init resized array
-                arr = np.zeros((res[1], res[0]))
-
-                # merge resized sub arrays
-                arr[0 : len(arr)//2] = arr_1
-                arr[len(arr)//2:] = arr_2
-
-                return arr
-            split_up_resize(roi, [28, 28])
-            #roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-            """ a pixel element is ‘1’ if atleast one pixel under the kernel is ‘1’.
-            So it increases the white region in the image or size of foreground object increases"""
+            roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+            #a pixel element is ‘1’ if atleast one pixel under the kernel is ‘1’.
+            #So it increases the white region in the image or size of foreground object increases
             roi = cv2.dilate(roi, (3, 3))
 
             # Calculate the HOG features
             roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
             nbr = classifier.predict(np.array([roi_hog_fd], 'float64'))
-            cv2.putText(frame, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+            print("Predicted number for ", c, " is: ", nbr[0])
+            cv2.putText(img, str(int(nbr[0])), (x, y),cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 255), 3)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
+print("Number of contours is: ", len(ctrs))
+cv2.imshow("Predicted digits within the image", img)
+ESC = 27
+while True:
+    keycode = cv2.waitKey()
+    if keycode != -1:
+        #keycode &amp;= 0xFF
+        if keycode == ESC:
+            break
 cv2.destroyAllWindows()
-
-#cv2.imshow("Predicted digits within the image", frame)
-#cv2.waitKey()
+exit()
